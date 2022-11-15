376654c9bc44cb5eeafa48d1f0bc072e9f5ab0c7
Mateus-OSilva <mateusods20@gmail.com>
2022-11-14 22:40:12 -0300

    Criação da var pontos

--- adivinhacao.py
+++ adivinhacao.py
@@ -5,11 +5,24 @@ print("*********************************")
 print("Bem vindo no jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = round(random.random() * 100)
+numero_secreto = random.randrange(1, 101)
 total_de_tentativas = 3
+pontos = 100
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Define o nível: "))
+if (nivel == 1):
+    total_de_tentativas = 20
+elif (nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {} ".format(rodada, total_de_tentativas))
+
     chute_str = input("Digite um número entre 1 e 100: ")
     print("Voce digitou ", chute_str)
     chute = int(chute_str)
@@ -24,13 +37,12 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if (acertou):
         print("Você acertou")
-
+        break
     else:
         if (maior):
             print("Você errou, o seu chute foi maior do que o número secreto.")
         elif (menor):
             print("Voce errou!, o seu chute foi menor do que o número secreto")
-
-    rodada = rodada + 1
-
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
     print("Fim do jogo")
