c2c3683892cb5288b3ff2881e72052eeb82751bf
Mateus-OSilva <mateusods20@gmail.com>
2022-11-14 22:21:04 -0300

    Criação do round(random.random)

--- adivinhacao.py
+++ adivinhacao.py
@@ -8,13 +8,13 @@ print("*********************************")
 numero_secreto = round(random.random() * 100)
 total_de_tentativas = 3
 
-for rodada in range(1, total_de_tentativas +1):
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {} ".format(rodada, total_de_tentativas))
     chute_str = input("Digite um número entre 1 e 100: ")
     print("Voce digitou ", chute_str)
     chute = int(chute_str)
 
-    if(chute < 1 or chute > 100):
+    if (chute < 1 or chute > 100):
         print("Você deve digitar um número entre 1 e 100!")
         continue
 
