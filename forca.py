bd06304d6672735e192ca56d360bba21ddb270ed
Mateus-OSilva <mateusods20@gmail.com>
2022-11-15 22:45:57 -0300

    var erros e laço de repetição

--- forca.py
+++ forca.py
@@ -10,6 +10,7 @@ def jogar():
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
@@ -18,11 +19,17 @@ def jogar():
         chute = input("Qual letra? ")
         chute = chute.strip()
         
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra
+                index = index + 1
+
+        else:
+            erros += 1
+        
+        enforcou = erros == 6
 
         print(letras_acertadas)
     
