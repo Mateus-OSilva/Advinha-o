97d2d35b8dcb8bfc3a7d03af14106bb0b89f29bb
Mateus-OSilva <mateusods20@gmail.com>
2022-11-15 21:47:37 -0300

    Criação do  laço for in e a var do while

--- forca.py
+++ forca.py
@@ -5,12 +5,19 @@ def jogar():
     print("***Bem vindo no jogo de Forca***!")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "python"
 
     enforcou = False
     acertou = False
 
     while(not enforcou and not acertou):
+
+        chute = input("QUal letra? ")
+
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print(letra)
+
         print("Jogando...")
     
     
