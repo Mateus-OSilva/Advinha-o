7b8c7ba48e7fc347b8bf291e399ae0133db2dc3a
Mateus-OSilva <mateusods20@gmail.com>
2022-11-15 22:48:32 -0300

    Editando o funcionamento para letras maiusculas

--- forca.py
+++ forca.py
@@ -5,7 +5,7 @@ def jogar():
     print("***Bem vindo no jogo de Forca***!")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
@@ -14,29 +14,27 @@ def jogar():
 
     print(letras_acertadas)
 
-    while(not enforcou and not acertou):
+    while (not enforcou and not acertou):
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
-        
-        if(chute in palavra_secreta):
+        chute = chute.strip().upper()
+
+        if (chute in palavra_secreta):
             index = 0
             for letra in palavra_secreta:
-                if(chute.upper() == letra.upper()):
+                if (chute.upper() == letra.upper()):
                     letras_acertadas[index] = letra
                 index = index + 1
 
         else:
             erros += 1
-        
+
         enforcou = erros == 6
 
         print(letras_acertadas)
-    
-    
+
     print("FIm do jogo")
 
-    
 
 if (__name__ == "__main__"):
     jogar()
